572c9fc51d283abd2b668021203f2eb308414e73
Everett Cinnamon <yaboihomer@gmail.com>
2025-01-31 18:03:38 -0500

    added user inputs for all constant variables

--- C3/C3.cpp
+++ C3/C3.cpp
@@ -7,12 +7,16 @@ using namespace std;
 
 int main()
 {
-    // Create and define variables
-    double purchasetotal = 95, statetax = 4, countytax = 2, totaltax;
+    // Create the variables
+    double purchasetotal, statetax, countytax, totaltax;
+
+    // Ask the user to define the variables
+    cout << "Please enter the purchase total:" << endl; cin >> purchasetotal;
+    cout << "Please enter the state tax, then the county tax:" << endl; cin >> statetax >> countytax;
 
     // Calculate the maff
     totaltax = (purchasetotal * (statetax / 100)) + (purchasetotal * (countytax / 100));
 
     // Display output to 2 decimal places
-    cout << fixed << setprecision(2) << "the total sales tax is " << totaltax << " and the final sale is " << purchasetotal + totaltax;
+    cout << fixed << setprecision(2) << "the total sales tax is " << totaltax << " and the final sale is " << purchasetotal + totaltax << endl;
 }
